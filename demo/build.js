9925116c3cfbd3eda0a2b164d130f832d298412e
glf <bluekylin@gmail.com>
2020-12-22 23:25:09 +0800

    feat: start development for cli

--- demo/build.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-const buildTool = require('./build-tool');
-let buildSrc = path.resolve(__dirname, './src/');
-let buildTarget = path.resolve(__dirname, './dist/');
-buildTool.run(buildSrc, buildTarget);
---
+++ demo/vue-scooter.config.js
@@ -0,0 +1,7 @@
+const path = require('path');
+module.exports = {
+  build: {
+    src: './src',
+    dist: './dist',
+  },
+};
