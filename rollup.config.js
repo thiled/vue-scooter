321d2addc9643efa1eca8407bb6f955f9f84fd55
gaolinfeng <bluekylin@gmail.com>
2021-07-21 21:00:13 +0800

    chore: output minified, add version info

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,24 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
+const version = require('./package.json').version;
+const versionStr = `vue-scooter v${version}`;
+const banner = `/* ${versionStr} */`;
 export default {
   input: 'src/index.js',
   output: {
     file: 'dist/vue-scooter.js',
     format: 'iife',
     name: 'VueScooter',
+    banner,
   },
-  plugins: [nodeResolve(), commonjs()],
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    uglify({
+      output: {
+        comments: `/${versionStr}/`,
+      },
+    }),
+  ],
 };
